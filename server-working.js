e73b538ce1f87eaeca42496ea68e022f8609d93c
ardalan1994 <mohajerrmohamad@gmail.com>
2024-10-04 23:19:20 +0330

    Add files via upload

--- server-working.js
+++ server-working.js
@@ -1,26 +1,49 @@
-const CACHE_NAME = 'attendance-cache-v1';
+// service-worker.js
+const CACHE_NAME = 'attendance-system-cache-v1';
 const urlsToCache = [
-    './index.html',
-    './script/app.js',
-    './style.css',
-    './icon-192x192.png',
-    './icon-512x512.png'
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    // لیست فایل‌هایی که باید کش شوند
 ];
 
+// نصب Service Worker
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
+                console.log('Caching files');
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
+// فعال‌سازی Service Worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetching from Cache
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
             .then((response) => {
-                return response || fetch(event.request);
+                if (response) {
+                    return response; // اگر فایل در کش موجود باشد، آن را برگردانید
+                }
+                return fetch(event.request); // در غیر این صورت، فایل را از سرور بارگیری کنید
             })
     );
 });
